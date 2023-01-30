ebb07407227d15f9f64e8a2fe5c66213333755de
mhmdkhlf <mohammadkhalifeh1010@gmail.com>
2023-01-30 11:47:43 +0200

    Added required modules for project and create an Express App that connect to my MongoDB database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,27 @@
-console.log('Hello from PitchPal');
+require('dotenv').config()
+
+import express from 'express';
+import mongoose from 'mongoose';
+
+// DB initialization and connection
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString!);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+// Starting up Express application
+const app = express();
+app.use(express.json());
+
+app.listen(5000, () => {
+    console.log('Hello from PitchPal')
+});
+
