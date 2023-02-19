ded38b663c3ad2fbbea118412678d0c13ea6f2c7
mhmdkhlf <mohammadkhalifeh1010@gmail.com>
2023-02-19 17:53:32 +0200

    added edit field by mongodb_id function

--- backend/src/controllers/field.controller.ts
+++ backend/src/controllers/field.controller.ts
@@ -28,5 +28,19 @@ async function getFieldsBySportCenterName(req: Request, res: Response) {
         res.status(400).json(error.message);
     }
 }
+async function updateFieldById(req: Request, res: Response) {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+        const result = await FieldModel.findByIdAndUpdate(
+            id, updatedData, options
+        )
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
 
-module.exports = { newField, getFieldsBySportCenterName};
+module.exports = { newField, getFieldsBySportCenterName, updateFieldById};
--- backend/src/routes/field.route.ts
+++ backend/src/routes/field.route.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/newField', fieldController.newField);
 router.get('/getFields/:sportCenterName',fieldController.getFieldsBySportCenterName)
+router.patch('/updateField/:id', fieldController.updateFieldById);
 
 module.exports = router;
