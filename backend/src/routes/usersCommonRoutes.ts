13c48d2ccc740f41249e484a7d0b82899116dcb0
mhmdkhlf <mohammadkhalifeh1010@gmail.com>
2023-03-11 19:36:20 +0200

    Delete usersCommonRoutes.ts

--- backend/src/routes/usersCommonRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const userController = require('../controllers/user-controller');
-
-const router = express.Router();
-
-router.post('/isFirstTimeLogIn', userController.isFirstTimeLogIn);
-
-module.exports = router;
