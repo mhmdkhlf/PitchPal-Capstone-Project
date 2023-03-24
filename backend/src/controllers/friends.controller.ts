a3fa74927fc6e293e6b3b5deeb82c5a049631d43
Ali Youssef Solh <alisolh1234@gmail.com>
2023-03-24 16:04:19 +0200

    added some useful apis for friends

--- backend/src/controllers/friends.controller.ts
+++ backend/src/controllers/friends.controller.ts
@@ -1,41 +1,67 @@
-import {Request, Response} from 'express';
-const FriendsModel = require('../models/friends.model.ts');
+import { Request, Response } from "express";
+import { stat } from "fs";
+const FriendsModel = require("../models/friends.model.ts");
 
 async function addFriend(req: Request, res: Response) {
-    const playerID1 = req.body.playerID1;
-    const playerID2 = req.body.playerID2;
-    let playerDoc1 = await FriendsModel.findOne({playerID: playerID1});
-    let playerDoc2 = await FriendsModel.findOne({playerID: playerID2});
-    if (playerDoc1) {
-        if (!playerDoc1.friendsIDs.includes(playerID2)) {
-            playerDoc1.friendsIDs.push(playerID2);
-        }
+  const playerID1 = req.body.playerID1;
+  const playerID2 = req.body.playerID2;
+  let playerDoc1 = await FriendsModel.findOne({ playerID: playerID1 });
+  let playerDoc2 = await FriendsModel.findOne({ playerID: playerID2 });
+  if (playerDoc1) {
+    if (!playerDoc1.friendsIDs.includes(playerID2)) {
+      playerDoc1.friendsIDs.push(playerID2);
     }
-    else {
-        playerDoc1 = new FriendsModel({
-            playerID: playerID1,
-            friendsIDs: [playerID2]
-        });
+  } else {
+    playerDoc1 = new FriendsModel({
+      playerID: playerID1,
+      friendsIDs: [playerID2],
+    });
+  }
+  if (playerDoc2) {
+    if (!playerDoc2.friendsIDs.includes(playerID1)) {
+      playerDoc2.friendsIDs.push(playerID1);
     }
-    if (playerDoc2) {
-        if (!playerDoc2.friendsIDs.includes(playerID1)) {
-            playerDoc2.friendsIDs.push(playerID1);
-        }
-    }
-    else {
-        playerDoc2 = new FriendsModel({
-            playerID: playerID2,
-            friendsIDs: [playerID1]
-        });
-    }
-    try {
-        const friendDoc1ToSave = await playerDoc1.save();
-        const friendDoc2ToSave = await playerDoc2.save();
-        res.status(200).json([friendDoc1ToSave, friendDoc2ToSave]);
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  } else {
+    playerDoc2 = new FriendsModel({
+      playerID: playerID2,
+      friendsIDs: [playerID1],
+    });
+  }
+  try {
+    const friendDoc1ToSave = await playerDoc1.save();
+    const friendDoc2ToSave = await playerDoc2.save();
+    res.status(200).json([friendDoc1ToSave, friendDoc2ToSave]);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+async function FriendshipStatus(req: Request, res: Response) {
+  const playerID1 = req.body.playerID1;
+  const playerID2 = req.body.playerID2;
+  const f1List = await FriendsModel.findOne({ playerID: playerID1 });
+  const f2List = await FriendsModel.findOne({ playerID: playerID2 });
+  let f1Friends = f1List.friendsIDs;
+  let f2Friends = f2List.friendsIDs;
+  var status = false;
+  if (f1Friends.includes(playerID2) && f2Friends.includes(playerID1)) {
+    status = true;
+  }
+  try {
+    res.status(200).json({ status });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+async function getNumberOfFriends(req: Request, res: Response) {
+  const playerID = req.params.playerID;
+  const fList = await FriendsModel.findOne({ playerID });
+  let fFriends = fList.friendsIDs;
+  let numberOfFriends = fFriends.length;
+  try {
+    res.status(200).json({ numberOfFriends });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
-module.exports = {addFriend};
+module.exports = { addFriend, FriendshipStatus, getNumberOfFriends };
--- backend/src/routes/friends.route.ts
+++ backend/src/routes/friends.route.ts
@@ -1,8 +1,13 @@
-import express from 'express';
-const friendController = require('../controllers/friends.controller');
+import express from "express";
+const friendController = require("../controllers/friends.controller");
 
 const router = express.Router();
 
-router.post('/addFriend', friendController.addFriend);
+router.post("/addFriend", friendController.addFriend);
+router.get(
+  "/getNumberOfFriends/:playerID",
+  friendController.getNumberOfFriends
+);
+router.post("/areFriends", friendController.FriendshipStatus);
 
 module.exports = router;
