f02994761fb9b4b3a1461886f327e86c6c0f1620
Ali Youssef Solh <alisolh1234@gmail.com>
2023-04-01 17:39:02 +0300

    update new sport center controller

--- backend/src/controllers/sport-center.controller.ts
+++ backend/src/controllers/sport-center.controller.ts
@@ -1,64 +1,69 @@
-import {Request, Response} from 'express';
+import { Request, Response } from "express";
 
-const SportCenterModel = require('../models/sport-center.model.ts');
+const SportCenterModel = require("../models/sport-center.model.ts");
 
 async function newSportCenter(req: Request, res: Response) {
-    const sportCenterData = new SportCenterModel({
-        name: req.body.name,
-        location: req.body.location,
-        phoneNumber: req.body.phoneNumber,
-        profilePicture: req.body.profilePicture,
-        linkToSocial: req.body.linkToSocial,
-        nbOfFields: req.body.nbOfFields,
-        facilityQualityAverageRating: req.body.facilityQualityAverageRating,
-        staffServiceAverageRating: req.body.staffServiceAverageRating,
-        nbOfRatings: req.body.nbOfRatings,
-        facilitiesAvailable: req.body.facilitiesAvailable,
-        workingHours: req.body.workingHours,
-    });
-    try {
-        const sportCenterToSave = await sportCenterData.save();
-        res.status(200).json(sportCenterToSave);
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  const sportCenterData = new SportCenterModel({
+    name: req.body.name,
+    location: req.body.location,
+    locationLink: req.body.locationLink,
+    phoneNumber: req.body.phoneNumber,
+    linkToFB: req.body.linkToFB,
+    linkToInsta: req.body.linkToInsta,
+    nbOfFields: req.body.nbOfFields,
+    facilityQualityAverageRating: req.body.facilityQualityAverageRating,
+    staffServiceAverageRating: req.body.staffServiceAverageRating,
+    nbOfRatings: req.body.nbOfRatings,
+    facilitiesAvailable: req.body.facilitiesAvailable,
+    workingHours: req.body.workingHours,
+  });
+  try {
+    const sportCenterToSave = await sportCenterData.save();
+    res.status(200).json(sportCenterToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
 async function getAllSportCenters(req: Request, res: Response) {
-    try{
-        const sportCenters = await SportCenterModel.find();
-        res.status(200).json(sportCenters);
-    }catch(error){
-        res.status(400).json(error.message);
-    }
+  try {
+    const sportCenters = await SportCenterModel.find();
+    res.status(200).json(sportCenters);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
 }
 
 async function updateSportCenterById(req: Request, res: Response) {
-    try {
-        const id = req.params.id;
-        const updatedData = req.body;
-        const options = { new: true };
-        const result = await SportCenterModel.findByIdAndUpdate(
-            id, updatedData, options
-        )
-        res.send(result);
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
+    const result = await SportCenterModel.findByIdAndUpdate(
+      id,
+      updatedData,
+      options
+    );
+    res.send(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
 async function deleteSportCenterById(req: Request, res: Response) {
-    try {
-        const id = req.params.id;
-        const dataInJson = await SportCenterModel.findByIdAndDelete(id);
-        const dataString = JSON.stringify(dataInJson);
-        res.send(`Deleted sport center with the following content:\n${dataString}`);
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
+  try {
+    const id = req.params.id;
+    const dataInJson = await SportCenterModel.findByIdAndDelete(id);
+    const dataString = JSON.stringify(dataInJson);
+    res.send(`Deleted sport center with the following content:\n${dataString}`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
-module.exports = { newSportCenter, getAllSportCenters, updateSportCenterById, deleteSportCenterById};
+module.exports = {
+  newSportCenter,
+  getAllSportCenters,
+  updateSportCenterById,
+  deleteSportCenterById,
+};
