fab46bf7049ca1c1022c1f3a18a375583a61b51e
mhmdkhlf <mohammadkhalifeh1010@gmail.com>
2023-04-02 04:18:42 +0300

    new sport center form frontend (view) is done. Still need to connect to backend and call API

---
+++ frontend_mobile/lib/components/time_input.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+// ignore: must_be_immutable
+class TimeInput extends StatefulWidget {
+  TimeInput({
+    super.key,
+    required this.timeInput,
+  });
+  TimeOfDay timeInput;
+
+  TimeOfDay get getTimeInput {
+    return timeInput;
+  }
+
+  @override
+  State<TimeInput> createState() => _TimeInputState();
+}
+
+class _TimeInputState extends State<TimeInput> {
+  @override
+  Widget build(BuildContext context) {
+    final hour = widget.timeInput.hour.toString().padLeft(2, '0');
+    final minute = widget.timeInput.minute.toString().padLeft(2, '0');
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        elevation: 5,
+        backgroundColor: kPrimaryColor,
+        shape: const StadiumBorder(),
+        side: const BorderSide(
+          width: 2,
+          color: kDarkGreen,
+        ),
+      ),
+      onPressed: () async {
+        TimeOfDay? newTime = await showTimePicker(
+          context: context,
+          initialTime: widget.timeInput,
+        );
+        if (newTime == null) return;
+        setState(() => widget.timeInput = newTime);
+      },
+      child: Text(
+        '$hour:$minute',
+        style: const TextStyle(
+          fontSize: 16,
+          color: kDarkGreen,
+        ),
+      ),
+    );
+  }
+}
