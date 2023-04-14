a3913c1641b315fd58d6a03d8e12ec349673ff81
mhmdkhlf <mohammadkhalifeh1010@gmail.com>
2023-04-14 04:44:53 +0300

    added api function to get player by their email

--- backend/src/routes/player.route.ts
+++ backend/src/routes/player.route.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/newPlayerProfile', playerController.createProfileInformation);
 router.patch('/updatePlayerInformation/:id', playerController.updatePlayerById);
 router.get('/getPlayer/:id', playerController.getPlayerInformation);
+router.get('/getPlayerByEmail/:email', playerController.getPlayerInformationByEmail);
 router.get('/getAllPlayers', playerController.getAllPlayers);
-
 
 module.exports = router;
